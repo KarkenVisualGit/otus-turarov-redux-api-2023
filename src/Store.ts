d9178338fc92c4ffe9c199c614e079d9e3f6f772
Karken Turarov <karken_turarov@mail.ru>
2023-11-15 12:08:31 +0600

    edit store

--- src/Store.ts
+++ src/Store.ts
@@ -1,43 +1,47 @@
 import { Action } from './types';
 
-export class Store {
-    private state;
-    private listeners = [];
-    private reducer;
-    private middlewares;
+type State = any;
+type Listener = () => void;
+type Middleware = (store: Store) => (next: Dispatch) => (action: Action) => void;
+type Dispatch = (action: Action) => void;
+type Reducer = (state: State, action: Action) => State;
 
-    constructor(reducer, initialState = {}, middlewares = []) {
+export class Store {
+    private state: State;
+    private listeners: Listener[] = [];
+    private reducer: Reducer;
+    private middlewares: Middleware[];
+
+    constructor(reducer: Reducer, initialState: State = {}, middlewares: Middleware[] = []) {
         this.reducer = reducer;
         this.state = initialState;
         this.middlewares = middlewares;
     }
 
-    getState() {
+    getState(): State {
         return this.state;
     }
 
-    dispatch(action: Action) {
-        this.middlewares.forEach(middleware => {
-            middleware(this)(action);
+    dispatch(action: Action): void {
+        const chain = this.middlewares.map(middleware => middleware(this));
+        let dispatch: Dispatch = this.rawDispatch.bind(this);
+        chain.reverse().forEach(middleware => {
+            dispatch = middleware(dispatch);
         });
 
-        this.state = this.reducer(this.state, action);
-        this.listeners.forEach(listener => listener());
 
-        return dispatch(action);
+        dispatch(action);
     }
 
-    subscribe(listener: () => void) {
+    private rawDispatch(action: Action): void {
+        this.state = this.reducer(this.state, action);
+        this.listeners.forEach(listener => listener());
+    }
+
+    subscribe(listener: Listener): () => void {
         this.listeners.push(listener);
         return () => {
             this.listeners = this.listeners.filter(l => l !== listener);
         };
     }
-
-    loggerMiddleware = store => next => action => {
-        console.log('dispatching', action);
-        let result = next(action);
-        console.log('next state', store.getState());
-        return result;
-    };
 }
