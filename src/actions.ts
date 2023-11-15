397dfcf1b3609e73eb4ea583d25b906c4f4cfe81
Karken Turarov <karken_turarov@mail.ru>
2023-11-15 09:55:24 +0600

    add actions

---
+++ src/actions.ts
@@ -0,0 +1,21 @@
+import { CartActionTypes, CartItem } from './types';
+
+export const addToCart = (item: CartItem) => ({
+  type: CartActionTypes.ADD_TO_CART,
+  payload: item
+});
+
+export const removeFromCart = (id: string) => ({
+  type: CartActionTypes.REMOVE_FROM_CART,
+  payload: id
+});
+
+export const selectProduct = (id: string) => ({
+  type: CartActionTypes.SELECT_PRODUCT,
+  payload: id
+});
+
+export const updateQuantity = (id: string, quantity: number) => ({
+  type: CartActionTypes.UPDATE_QUANTITY,
+  payload: { id, quantity }
+});
