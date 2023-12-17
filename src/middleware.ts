f81c4a99bcc48e47c5bee6e5cacb40a6d560770b
Karken Turarov <karken_turarov@mail.ru>
2023-12-17 18:03:09 +0600

    edit tests  lint fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,11 @@ import { Action } from "./types";
 export const loggerMiddleware =
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (store: Store) => (next: (action: Action) => void) => (action: Action) => {
+    // eslint-disable-next-line no-console
+    console.log("dispatching", action);
     const result = next(action);
+    // eslint-disable-next-line no-console
+    console.log("next state", store.getState());
     return result;
   };
 
